db24da871f2741b079a275ce3f56f2a18695366a
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-02-22 21:12:30 -0800

    got test passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { DarkModeProvider } from './context/DarkModeContext';
 import { EntryProvider } from './context/EntryContext';
 import { UserProvider } from './context/UserContext';
 
 test('input changes value, then button renders input', () => {
   render(
-    <UserProvider>
-      <EntryProvider>
-        <App />
-      </EntryProvider>
-    </UserProvider>
+    <DarkModeProvider>
+      <UserProvider>
+        <EntryProvider>
+          <App />
+        </EntryProvider>
+      </UserProvider>
+    </DarkModeProvider>
   );
 
   const name = screen.getByLabelText(/name/i);
